b33ee85c1394bb93060b20836043b634860d729e
Laszlo Lazuer <visualjazz@gmail.com>
2024-07-26 13:52:09 -0600

    feat: app looks good now.

--- src/NameModal.jsx
+++ src/NameModal.jsx
@@ -8,14 +8,14 @@ const NameModal = ({ onSave }) => {
     if (name.length >= 3) {
       onSave(name);
     } else {
-      alert('Name must be at least 3 characters long.');
+      alert('Initials must be at least 3 characters long.');
     }
   };
 
   return (
     <div className="modal">
       <div className="modal-content">
-        <h2>Enter your name</h2>
+        <h2>Enter your Initials</h2>
         <input
           type="text"
           value={name}
